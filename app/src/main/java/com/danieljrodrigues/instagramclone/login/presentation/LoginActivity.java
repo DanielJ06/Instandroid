e00228898a04aea27acc20adb827853579d41c20
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-02-20 18:59:02 -0300

    Button states

--- app/src/main/java/com/danieljrodrigues/instagramclone/login/presentation/LoginActivity.java
+++ app/src/main/java/com/danieljrodrigues/instagramclone/login/presentation/LoginActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.danieljrodrigues.instagramclone.R;
@@ -11,17 +14,54 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button submitBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextInputLayout inputLayout = findViewById(R.id.login_edit_text_email_input);
-        inputLayout.setError("Invalid email");
+        editTextEmail = findViewById(R.id.login_edit_text_email);
+        editTextPassword = findViewById(R.id.login_edit_text_password);
+        submitBtn = findViewById(R.id.login_button_submit);
 
-        EditText editText = findViewById(R.id.login_edit_text_email);
-        editText.setBackground(ContextCompat.getDrawable(
-            LoginActivity.this, R.drawable.edit_text_error_bg
-        ));
+        editTextEmail.addTextChangedListener(watcher);
+        editTextPassword.addTextChangedListener(watcher);
+        submitBtn.setOnClickListener(v -> {
+            TextInputLayout emailInputLayout = findViewById(R.id.login_edit_text_email_input);
+            emailInputLayout.setError("Erro email inválido");
+            editTextEmail.setBackground(ContextCompat.getDrawable(
+                LoginActivity.this, R.drawable.edit_text_error_bg)
+            );
+
+            TextInputLayout passwdInputLayout = findViewById(R.id.login_edit_text_password_input);
+            passwdInputLayout.setError("Erro senha inválido");
+            editTextPassword.setBackground(ContextCompat.getDrawable(
+                LoginActivity.this, R.drawable.edit_text_error_bg)
+            );
+        });
     }
+
+    private TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!s.toString().isEmpty()) {
+                submitBtn.setEnabled(true);
+            } else {
+                submitBtn.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
