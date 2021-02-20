8a189064e615d82c63fbb5a0bb6aac88de81e969
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-02-20 10:20:53 -0300

    Inputs validations

--- app/src/main/java/com/danieljrodrigues/instagramclone/login/presentation/LoginActivity.java
+++ app/src/main/java/com/danieljrodrigues/instagramclone/login/presentation/LoginActivity.java
@@ -1,16 +1,27 @@
 package com.danieljrodrigues.instagramclone.login.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.danieljrodrigues.instagramclone.R;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputLayout inputLayout = findViewById(R.id.login_edit_text_email_input);
+        inputLayout.setError("Invalid email");
+
+        EditText editText = findViewById(R.id.login_edit_text_email);
+        editText.setBackground(ContextCompat.getDrawable(
+            LoginActivity.this, R.drawable.edit_text_error_bg
+        ));
     }
 }
