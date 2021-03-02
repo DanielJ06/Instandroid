f9b96b82255713e6b8baf6b21917bc4df5858270
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-01 21:58:08 -0300

    Custom toolbar

--- app/src/main/java/com/danieljrodrigues/instagramclone/main/presentation/MainActivity.java
+++ app/src/main/java/com/danieljrodrigues/instagramclone/main/presentation/MainActivity.java
@@ -1,8 +1,15 @@
 package com.danieljrodrigues.instagramclone.main.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.danieljrodrigues.instagramclone.R;
 
@@ -12,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            Drawable drawable = getResources().getDrawable(R.drawable.ic_insta_camera);
+            getSupportActionBar().setHomeAsUpIndicator(drawable);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 }
